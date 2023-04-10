805461709091e3639b001db40d431300324012c9
Aremu-olusegun <Aremugraphics@gmail.com>
2023-04-09 18:19:02 -0700

    3-mul.c code completed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,60 +1,32 @@
-#include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - prints the product of two integers to the stdout
- * @argc: the number of passed arguments
- * @argv: the array of passed arguments
- *
- * Return: the product if argc == 2, else 1
+ * main - Program that takes two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int result, i, j;
+	int i, j;
 
-	if (argc == 3)
+	if (argc == 1 || argc == 2)
 	{
-		i = _atoi(argv[1]);
-		j = _atoi(argv[2]);
-		result = i * j;
-		printf("%d\n", result);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	puts("Error");
-	return (1);
-}
-
-/**
- * _atoi - extracts integer from string
- * @s: the string to be parsed
- *
- * Return: integer if integer is present in s, else 0
- */
-int _atoi(char *s)
-{
-	int FLAG = 0, len, n = 0, d = 0, digit, i;
-
-	for (len = 0; s[len] != '\0'; len++)
-		;
-
-	for (i = 0; i < len && !FLAG; i++)
+	else
 	{
-		if (s[i] == '-')
-			d++;
+		j = 1;
 
-		if (s[i] >= 48 && s[i] <= 57)
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			FLAG = 1;
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
-			FLAG = 0;
-		}
+		for (i = 1; i < 3; i++)
+		j *= atoi(argv[i]);
+
+		printf("%d\n", j);
 	}
-	if (!FLAG)
-		return (0);
-	return (n);
+
+	return (0);
 }
