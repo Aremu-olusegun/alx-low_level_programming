94078b14e2a92f086233f269bf8fa82de432e14d
Aremu-olusegun <Aremugraphics@gmail.com>
2023-05-02 03:37:03 -0700

    task 2 completed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * add_node - adds a new node at the beginning of a list
- *@head: turn the pointer to the head of the list
+ *@head: pointer to the head of the list
  *@str: string to be added
  *
  * Return: returns the address to the new element or NULL
@@ -13,16 +13,16 @@ list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
 	int len;
-	list_t *new_node;
+	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	dup = strdup(str);
 	if (dup == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
 	for (len = 0; str[len];)
@@ -32,8 +32,8 @@ list_t *add_node(list_t **head, const char *str)
 	new->len = len;
 	new->next = *head;
 
-	*head = new_node;
+	*head = new;
 
-	return (new_node);
+	return (new);
 
 }
