161c8fffdb215b3b066e3a9b8a8f3965ec27d227
Aremu-olusegun <Aremugraphics@gmail.com>
2023-05-02 07:36:56 -0700

    third commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 
 /**
-  *remove_list_element - removes the node at given index
-  *@head: pointer to the head of the linked list
-  *@index: the index of the node to be deleted
+  *remove_node_at_index - Removes a node at the given index from the linked list
+  *@head: Pointer to the head of the linked list
+  *@index: Index of the node to be removed
   *Return: 1 on success, -1 on failure
   */
-int remove_list_element(listint_t **head, unsigned int index)
+int remove_node_at_index(listint_t **head, unsigned int index)
 {
 unsigned int i;
 listint_t *current_node, *next_node;
-
 if (!head || !*head)
 return (-1);
 
@@ -29,7 +28,6 @@ current_node = current_node->next;
 if (current_node == NULL)
 return (-1);
 }
-
 next_node = current_node->next;
 current_node->next = next_node->next;
 free(next_node);
