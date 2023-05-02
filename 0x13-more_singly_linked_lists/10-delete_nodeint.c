427015880c16c6225b455fb13f501bf6f77fe5c4
Aremu-olusegun <Aremugraphics@gmail.com>
2023-05-02 07:49:09 -0700

    10-delete_nodeint.c task completed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,35 +1,37 @@
 #include "lists.h"
 
 /**
-  *remove_node_at_index - Removes a node at the given index from the linked list
-  *@head: Pointer to the head of the linked list
-  *@index: Index of the node to be removed
+  *remove_node_from_list_at_index - remove node from list at index
+  *@head: pointer to head of list
+  *@index: index of node to remove
   *Return: 1 on success, -1 on failure
   */
-int remove_node_at_index(listint_t **head, unsigned int index)
+int remove_node_from_list_at_index(listint_t **head, unsigned int index)
 {
 unsigned int i;
-listint_t *current_node, *next_node;
+listint_t *current, *next;
+
 if (!head || !*head)
 return (-1);
 
-current_node = *head;
+current = *head;
 
 if (index == 0)
 {
 *head = (*head)->next;
-free(current_node);
+free(current);
 return (1);
 }
 
 for (i = 0; i < (index - 1); i++)
 {
-current_node = current_node->next;
-if (current_node == NULL)
+current = current->next;
+if (current == NULL)
 return (-1);
 }
-next_node = current_node->next;
-current_node->next = next_node->next;
-free(next_node);
+
+next = current->next;
+current->next = next->next;
+free(next);
 return (1);
 }
