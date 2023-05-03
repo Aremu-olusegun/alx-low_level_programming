4a3393eb020f728b14927acbc2cc56d4c614e41c
Aremu-olusegun <Aremugraphics@gmail.com>
2023-05-03 16:50:17 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,33 +1,49 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t list
- *
- * @h: listint_t double pointer
- *
- * Return: The size of the list that was freed
- */
+* my_free_listint_safe - Frees a listint_t list
+*
+* @h: listint_t double pointer
+*
+* Return: size_t
+*/
 
-size_t free_listint_safe(listint_t **h)
+size_t my_free_listint_safe(listint_t **h)
 {
-    size_t size = 0;
-    listint_t *current, *next;
+	/* Initialize variables */
+	size_t count = 0;
+	int count2;
+	listint_t *head, *new_node;
 
-    if (h == NULL || *h == NULL)
-        return (0);
+	/* Check if the list is empty */
+	if (*h == NULL)
+	{
+		return (0);
+	}
 
-    current = *h;
+	/* Assign head to the pointer */
+	head = *h;
 
-    while (current != NULL)
-    {
-        size++;
-        next = current->next;
-        free(current);
-        if (next >= current)
-            break;
-        current = next;
-    }
+	/* Traverse the list */
+	while (head)
+	{
+		count++;
+		count2 = head->next - head;
 
-    *h = NULL;
-    return (size);
+		/* Free the current node */
+		new_node = head;
+		free(new_node);
+
+		/* Check if the nodes are consecutive */
+		if (count2 >= 0)
+		{
+			break;
+		}
+
+		/* Move the pointer to the next node */
+		head = head->next;
+	}
+
+	return (count);
 }
+
