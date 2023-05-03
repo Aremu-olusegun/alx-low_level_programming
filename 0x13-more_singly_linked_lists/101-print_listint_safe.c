f94d86ae1c30ee766db06ae315c2481196f50e1d
Aremu-olusegun <Aremugraphics@gmail.com>
2023-05-03 16:00:42 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,37 +1,42 @@
 #include "lists.h"
 
 /**
-* free_listint_safe - Frees a listint_t list
+* print_listint_safe - Prints a listint_t linked list
 *
-* @h: listint_t double pointer
+* @head: listint_t head
 *
 * Return: size_t
 */
 
-size_t free_listint_safe(listint_t **h)
+
+size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	int count2;
-	listint_t *head, *new_node;
+	size_t count1 = 0, count2;
+	const listint_t *temp = head;
+	const listint_t *next_node;
 
-	if (*h == NULL)
-	{
-		return (0);
-	}
+	do {
+		printf("[%p] %d\n", (void *)temp, temp->n);
 
-	head = *h;
+		count1++;
 
-	for (; head != NULL; head = head->next)
-	{
-		count++;
-		count2 = head->next - head;
-		new_node = head;
-		free(new_node);
+		temp = temp->next;
+		next_node = head;
 
-		if (count2 >= 0)
-		{
-			break;
-		}
-	}
-	return (count);
+		count2 = 0;
+
+		do {
+			if (temp == next_node)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (count1);
+			}
+
+			next_node = next_node->next;
+			count2++;
+		} while (count2 < count1);
+
+	} while (temp != NULL);
+
+	return (count1);
 }
