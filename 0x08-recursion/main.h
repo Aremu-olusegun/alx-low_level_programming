290cc6b2b484bd78dd28b7fc09e1721d13c7351c
Aremu-olusegun <Aremugraphics@gmail.com>
2023-06-08 02:48:39 -0700

    changes made

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,7 @@
 *char prototypes(char) 
 */
 void _puts_recursion(char *s);
- void _print_rev_recursion(char *s);
+ void _print_reverse_recursion(char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
