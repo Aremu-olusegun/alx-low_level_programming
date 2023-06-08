e7fb73af83773f3c337abb8a504c2e3155b92103
Aremu-olusegun <Aremugraphics@gmail.com>
2023-06-08 02:58:39 -0700

    description updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _puts_recursion - prints a string followed by a new line
-*@s: character to be printed
+*@s1: character to be printed
 *Return: returns void
 */
 void _puts_recursion(char *s1)
